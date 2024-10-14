746757a35f56049e9e8518c2a40a0dc82b43c3b6
Tylko-jedno <diogkek@gmail.com>
2024-10-14 16:35:34 +0300

    first master commit

---
+++ Lab06_3rec/Lab06_3rec.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int f(int* a, const int size, int i, int res) {
+    if (i > size - 1)
+        return res;
+    else {
+        if (a[i] % 2 == 0 && a[i] > res)
+            return f(a, size, i + 1, a[i]);
+        else
+            return f(a, size, i + 1, res);
+    }
+}
+
+template <typename T, typename T2>
+T f2(T2* a, const T size, T i, T res) {
+    if (i > size - 1)
+        return res;
+    else {
+        if (a[i] % 2 == 0 && a[i] > res)
+            return f(a, size, i + 1, a[i]);
+        else
+            return f(a, size, i + 1, res);
+    }
+}
+
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+    a[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(a, size, Low, High, i + 1);
+}
+
+
+void Print(int* a, const int size, int i)
+{
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+
+int main()
+{
+    int c;
+    cout << "n = "; cin >> c;
+
+    int* a = new int[c];
+
+    Create(a, c, -10, 10, 0);
+    Print(a, c, 0);
+
+    int res = f(a, c, 0, -10);
+    cout << "res = " << res << endl;
+    res = f2(a, c, 0, -10);
+    cout << "res = " << res << endl;
+
+    delete[] a;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab06_3rec/Lab06_3rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[22];
+
+			Create(a, 22, -10, 35, 0);
+
+			int expected[22] = { 31,  11,  22, -6,  23,  28,  14,   0, -4,  28, -9,  29, -5,  27,  15,  21, -5,  18,  33, -2, -3,  12 };
+
+			for (int i = 0; i < 22; i++)
+			{
+				Assert::AreEqual(expected[i], a[i], L"Elements are not equal", LINE_INFO());
+			}
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int a[22] = { 10, -2,   3,   9,   7,   6,   2, -10 };
+			int expected = 10;
+			int res = f(a, 8, 0, 10);
+
+
+			Assert::AreEqual(expected, res, 0.0000001);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			int a[22] = { 10, -2,   3,   9,   7,   6,   2, -10 };
+			int expected = 10;
+			int res = f2(a, 8, 0, 10);
+
+
+			Assert::AreEqual(expected, res, 0.0000001);
+		}
+	};
+}
